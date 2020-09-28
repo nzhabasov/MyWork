8a56746155395aa82fa0fb767c30900c7540a7b7
nzhabasov <zhnurik_02@mail.ru>
2020-09-28 08:25:51 +0600

    commit good work 3

---
+++ Goodwork2/main.py
@@ -0,0 +1,49 @@
+# типы данных языка Python
+
+# x = 13      # integer
+# y = 1.2     # floating point number
+
+# myString = 'myString value'
+# myString2 = "myString2 value"
+
+# question = 152.654
+
+# mySet = {13, 14, 16, 98, 13}    # множество
+# myTuple = (15, 46, 46, 48)      # пара
+# myList = [64, 7, 49, 64]        # вектор
+
+# print(myTuple)
+
+# с = set(myTuple)
+# print(с)
+
+# r = tuple(с)
+# print(r)
+
+# print(x*y)      # умножение
+# print(x+y)      # сложение
+# print(x/y)      # деление
+# print(x-y)      # вычитание
+
+
+# print(type(с))  # выводит тип данных переменной
+
+# ДЗ: 1. Попробуйте сохранить в одной переменной следующий вектор: 13, 46, 79, 85, 95.
+#     2. Реализуйте умножение вектора из 1 на: 100, 34, 46, 52, 34.
+
+# 1 Сделанно
+x = (100, 34, 46, 52, 34)
+
+print(x)
+
+
+myList = [13, 46, 79, 85, 95]
+
+print(myList)
+
+print(x*myList)
+
+
+
+
+
---
+++ Goodwork3/main.py
@@ -0,0 +1,14 @@
+# for"* .\n""** .\n""*** .\n""**** .\n"
+
+#for * in ** in *** in ****
+#   print(*)
+
+
+languages = ["C", "C+", "Perl", "Python"]
+
+for language in languages:
+    if language == 'Python':
+        print(language' очень крутой')
+    else:
+        print(language"не так крут,как Python")
+
